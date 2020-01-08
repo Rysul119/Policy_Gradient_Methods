293600cac2bca31a6415899c1e3ec580a4721f83
rysul119 <rysulbuet12@gmail.com>
2020-01-08 13:31:14 -0600

    Fixed added saving options for the episodic score and plotting

--- REINFORCEMENT.py
+++ REINFORCEMENT.py
@@ -5,23 +5,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(32, input_dim = 4, activation='relu'))
+model.add(tf.keras.layers.Dense(64, input_dim = 4, activation='relu'))
 model.add(tf.keras.layers.Dense(2, activation = "softmax"))
 model.build()
-optimizer = tf.keras.optimizers.Adam(learning_rate = 0.01)
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
 
 env = gym.make('CartPole-v1')
 episodes = 10000
-
-score = 0
 steps = 1000
 gamma = 0.98
+env.seed(10)
+np.random.seed(5)
+
+scores = []
 
 for e in range(episodes+1):
     s = env.reset()
-    ep_memory = []
     history = []
-    #ep_score = 0
+    ep_score = 0
     done = False
     for step in range(steps+1):
         s = s.reshape([1, 4])
@@ -30,29 +31,29 @@ for e in range(episodes+1):
         act = np.random.choice(range(env.action_space.n), p=act_prob.numpy()[0])
         next_s, r, done, _ = env.step(act)
         history.append((s, act, r))
-        score += r
+        ep_score += r
         s = next_s
         if done:
+            scores.append(ep_score)
             R = 0
             for s, act, r in history[::-1]:
                 s = s.reshape([1, 4])
                 R = r + gamma * R
                 with tf.GradientTape() as tape:
                     output = model(s)
-                    output = tf.reshape(output, [-1])
-                    actProbs = output[act]
-                    #actProbs = tf.gather(tf.reshape(output, [-1]), act)
+                    #output = tf.reshape(output, [-1])
+                    #actProbs = output[act]
+                    actProbs = tf.gather(tf.reshape(output, [-1]), act)
                     loss = -tf.reduce_mean(tf.math.log(actProbs) * R)
                 grads = tape.gradient(loss, model.trainable_variables)
                 optimizer.apply_gradients(zip(grads, model.trainable_variables))
             break
-    if e % 50 == 0:
-        print("Episode {} avg steps: {}".format(e, score/50))
-        score = 0
+    if (e+1) % 100 == 0:
+        print("Episode {} avg steps: {}".format(e+1, np.mean(scores[-100:])))
 
-dict = {'Score': ep_memory}
+dict = {'Score': scores}
 df = pd.DataFrame(dict)
-df.to_csv('Ep_score.csv')
+df.to_csv('Reinforce1.csv')
 
 x = range(1, episodes+1)
 plt.scatter(x, df['Score'])
