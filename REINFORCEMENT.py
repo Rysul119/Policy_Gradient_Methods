9572acbdcaaed7528618f6b8ef8be09808902eb2
rysul119 <rysulbuet12@gmail.com>
2020-01-08 17:08:17 -0600

    Fixed episode count

--- REINFORCEMENT.py
+++ REINFORCEMENT.py
@@ -9,7 +9,6 @@ model.add(tf.keras.layers.Dense(64, input_dim = 4, activation='relu'))
 model.add(tf.keras.layers.Dense(2, activation = "softmax"))
 model.build()
 optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
-
 env = gym.make('CartPole-v1')
 episodes = 10000
 steps = 1000
@@ -19,24 +18,24 @@ np.random.seed(5)
 
 scores = []
 
-for e in range(episodes+1):
+for e in range(episodes):
     s = env.reset()
-    history = []
+    hist = []
     ep_score = 0
     done = False
-    for step in range(steps+1):
+    for step in range(steps):
         s = s.reshape([1, 4])
         act_prob = model(s)
 
         act = np.random.choice(range(env.action_space.n), p=act_prob.numpy()[0])
         next_s, r, done, _ = env.step(act)
-        history.append((s, act, r))
+        hist.append((s, act, r))
         ep_score += r
         s = next_s
         if done:
             scores.append(ep_score)
             R = 0
-            for s, act, r in history[::-1]:
+            for s, act, r in hist[::-1]:
                 s = s.reshape([1, 4])
                 R = r + gamma * R
                 with tf.GradientTape() as tape:
@@ -53,7 +52,7 @@ for e in range(episodes+1):
 
 dict = {'Score': scores}
 df = pd.DataFrame(dict)
-df.to_csv('Reinforce1.csv')
+df.to_csv('Reinforce.csv')
 
 x = range(1, episodes+1)
 plt.scatter(x, df['Score'])
